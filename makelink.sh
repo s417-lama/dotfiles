87e2ee2ea2729c42a816a9d5272992699694c63d
slama <s417.lama@gmail.com>
2017-10-07 12:51:29 +0900

    add gitignore_global and its config

--- makelink.sh
+++ makelink.sh
@@ -4,6 +4,8 @@ DIR=$(cd $(dirname $0) && pwd)
 
 ln -sf $DIR/.vimrc ~/.vimrc
 ln -sf $DIR/.tmux.conf ~/.tmux.conf
+ln -sf $DIR/.gitignore_global ~/.gitignore_global
+ln -sf $DIR/.gitconfig ~/.gitconfig
 ln -snf $DIR/.vim/colors ~/.vim
 ln -snf $DIR/.tmux/tmuxline.conf ~/.tmux/tmuxline.conf
 ln -snf $DIR/.config/fish/config.fish ~/.config/fish/config.fish
