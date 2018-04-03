95f2ef16402ff00187518db55e5633e9d53e4e36
shiina <shiina@usernombp.tok.access-company.com>
2018-04-03 15:25:00 +0900

    make dir in makelink.sh

--- makelink.sh
+++ makelink.sh
@@ -2,13 +2,17 @@
 
 DIR=$(cd $(dirname $0) && pwd)
 
-ln -sf $DIR/.vimrc ~/.vimrc
 ln -sf $DIR/.tmux.conf ~/.tmux.conf
 ln -sf $DIR/.gitignore_global ~/.gitignore_global
 ln -sf $DIR/.gitconfig ~/.gitconfig
-ln -snf $DIR/.vim/colors ~/.vim
+mkdir -p ~/.config
 ln -snf $DIR/.vim/colors ~/.config/nvim
+mkdir -p ~/.tmux
 ln -snf $DIR/.tmux/tmuxline.conf ~/.tmux/tmuxline.conf
+mkdir -p ~/.config/fish
 ln -snf $DIR/.config/fish/config.fish ~/.config/fish/config.fish
+mkdir -p ~/.cgdb
 ln -snf $DIR/.cgdb/cgdbrc ~/.cgdb/cgdbrc
+mkdir -p ~/.config/nvim
 ln -snf $DIR/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -snf $DIR/.vim/colors ~/.config/nvim/colors
