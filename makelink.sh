cacb76be0c5e3a43f944943d0c54baab049539af
slama <s417.lama@gmail.com>
2018-04-09 22:46:15 +0900

    add .Xmodmap and fix problem with makelink.sh

--- makelink.sh
+++ makelink.sh
@@ -5,14 +5,13 @@ DIR=$(cd $(dirname $0) && pwd)
 ln -sf $DIR/.tmux.conf ~/.tmux.conf
 ln -sf $DIR/.gitignore_global ~/.gitignore_global
 ln -sf $DIR/.gitconfig ~/.gitconfig
-mkdir -p ~/.config
+mkdir -p ~/.config/nvim
 ln -snf $DIR/.vim/colors ~/.config/nvim
 mkdir -p ~/.tmux
-ln -snf $DIR/.tmux/tmuxline.conf ~/.tmux/tmuxline.conf
+ln -sf $DIR/.tmux/tmuxline.conf ~/.tmux/tmuxline.conf
 mkdir -p ~/.config/fish
-ln -snf $DIR/.config/fish/config.fish ~/.config/fish/config.fish
+ln -sf $DIR/.config/fish/config.fish ~/.config/fish/config.fish
 mkdir -p ~/.cgdb
-ln -snf $DIR/.cgdb/cgdbrc ~/.cgdb/cgdbrc
+ln -sf $DIR/.cgdb/cgdbrc ~/.cgdb/cgdbrc
 mkdir -p ~/.config/nvim
-ln -snf $DIR/.config/nvim/init.vim ~/.config/nvim/init.vim
-ln -snf $DIR/.vim/colors ~/.config/nvim/colors
+ln -sf $DIR/.config/nvim/init.vim ~/.config/nvim/init.vim
