23aa8ab6f3c0e6a32255acce593f89fe3a575dcc
slama <s417.lama@gmail.com>
2019-06-07 20:40:06 +0900

    revert config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -82,6 +82,3 @@ set -xU LESS_TERMCAP_se (printf "\e[0m")
 set -xU LESS_TERMCAP_so (printf "\e[01;40;33m")
 set -xU LESS_TERMCAP_ue (printf "\e[0m")
 set -xU LESS_TERMCAP_us (printf "\e[01;31m")
-
-source ~/.asdf/asdf.fish
-set -gx PATH (asdf where elixir)/.mix/escripts $PATH
